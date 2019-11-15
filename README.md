bbbcf9e1840a906e1496d7e14fdb1299b0d810c3
Flavien <flavien.martineau@etu.u-bordeaux.fr>
2019-11-15 21:33:50 +0100

    Login/Register form view added + bootstrap -> ng-bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WebProjectM2
+
+Pour lancer le front, aller dans le dossier front et tapez : 
+
+`npm install`
+`ng serve --open`
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule }   from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgSelectModule} from '@ng-select/ng-select';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    NgSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
