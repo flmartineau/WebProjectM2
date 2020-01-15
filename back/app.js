fd68a16d1d209cbc7b2e791525380cd68b718baa
Flavien <flavien.martineau@etu.u-bordeaux.fr>
2020-01-15 13:44:57 +0100

    Update port config

--- back/app.js
+++ back/app.js
@@ -7,11 +7,12 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const jwtVerify = require('./config/jwt.verify') ;
 const cors = require('cors');
 const app = express();
 const passport = require('passport');
-
+const port = process.env.PORT || 3002;
 const userRouter = require('./routers/user.router');
 const projectRouter = require('./routers/project.router');
 
@@ -23,6 +24,8 @@ app.use(passport.initialize());
 app.use('/api', userRouter);
 app.use('/api/project', jwtVerify.verifyJwtToken, projectRouter);
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
 
 app.use((err, req, res, next) => {
     if (err.name === 'ValidationError') {
@@ -32,8 +35,8 @@ app.use((err, req, res, next) => {
     }
 });
 
-app.listen(process.env.PORT || 3002, () => {
-    console.log('Démarrage du serveur sur le port :' + process.env.PORT);
+app.listen(port, () => {
+    console.log('Démarrage du serveur sur le port :' + port);
 });
 
 module.exports = app;
--- back/config/config.js
+++ back/config/config.js
@@ -1,7 +1,4 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json');
-const envConfig = config[env];
+
 process.env.JWT_SECRET = process.env.JWT_SECRET || 'SECRETJWTM2';
 process.env.JWT_EXP = process.env.JWT_EXP || '1h';
-
-Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
