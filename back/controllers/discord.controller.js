214f9275551b2bc52a00ab2f9d184fc20a209bb2
Flavien <flavien.martineau@etu.u-bordeaux.fr>
2020-01-20 12:28:26 +0100

    Fix controllers

--- back/controllers/discord.controller.js
+++ back/controllers/discord.controller.js
@@ -9,20 +9,26 @@ const APIReference = mongoose.model('APIReference');
  * Update the discord reference of the project.
  */
 module.exports.updateDiscord = (req, res, next) => {
-    const discordServer = new APIReference();
-    discordServer.link = req.body.discordServerID+"/"+req.body.discordChannelID;
-    //discordServer.tokenAPI = null;
-    //discordServer.usernameAPI = null;
-    discordServer.save().then(() => {
-        Project.findOne({ _id: req.params.projectId }, (err, project) => {
-            if (!project) res.status(404).json({ status: false, message: 'Project not found.' });
-            else {
-                project.discord = discordServer;
-                project.save(function (err) {
-                    if (!err)
+    console.log(req.body)
+
+    Project.findOne({ _id: req.params.projectId }, (err, project) => {
+        if (!project) res.status(404).json({ status: false, message: 'Project not found.' });
+        else {
+
+            APIReference.findOne({ _id: project.discord._id }, (err, discordServer) => {
+                discordServer.link = req.body.discordServerID+"/"+req.body.discordChannelID;
+                //discordServer.tokenAPI = null;
+                //discordServer.usernameAPI = null;
+                discordServer.save().then(
+                    () => {
                         res.status(204).send({ success: 'Discord updated with success.' });
-                });
-            }
-        })
-    });
+                    }
+                ).catch(
+                    (error) => {
+                        res.status(400).json({ error: error });
+                    }
+                );
+            });
+        }
+    })
 };
--- back/controllers/trello.controller.js
+++ back/controllers/trello.controller.js
@@ -8,21 +8,26 @@ const APIReference = mongoose.model('APIReference');
  * Update the trello reference of the project.
  */
 module.exports.updateTrello = (req, res, next) => {
-    const trello = new APIReference();
-    trello.link = req.body.trelloBoardUrl;
-    trello.tokenAPI = req.body.trelloToken;
-    trello.usernameAPI = req.body.trelloKey;
 
-    trello.save().then(() => {
-        Project.findOne({ _id: req.params.projectId }, (err, project) => {
-            if (!project) res.status(404).json({ status: false, message: 'Project not found.' });
-            else {
-                project.trello = trello;
-                project.save(function (err) {
-                    if (!err)
+    Project.findOne({ _id: req.params.projectId }, (err, project) => {
+        if (!project) res.status(404).json({ status: false, message: 'Project not found.' });
+        else {
+
+            APIReference.findOne({ _id: project.trello._id }, (err, trello) => {
+                trello.link = req.body.trelloBoardUrl;
+                trello.tokenAPI = req.body.trelloToken;
+                trello.usernameAPI = req.body.trelloKey;
+                trello.save().then(
+                    () => {
                         res.status(204).send({ success: 'Trello updated with success.' });
-                });
-            }
-        })
-    });
+                    }
+                ).catch(
+                    (error) => {
+                        res.status(400).json({ error: error });
+                    }
+                );
+            });
+        }
+    })
+
 };
